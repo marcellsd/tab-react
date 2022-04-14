9e480efa6f8580b221f3ff20f1d8a8eceeb59d37
Marcell Santana de Deus <marcellsantana@gmail.com>
2022-04-14 15:24:03 -0300

    add tab-react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Tab } from './components/Tab';
+
+function App() {
+  return (
+    <Tab />
+  );
+}
+
+export default App;
---
+++ src/components/Tab.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import { TabItem } from "./TabItens";
+
+
+const tabsJSON = [
+    {
+        tabName: 'tab 1',
+        tabText: 'Texto do primeiro tab'
+    },
+    {
+        tabName: 'tab 2',
+        tabText: 'Texto do segundo tab'
+    },
+    {
+        tabName: 'tab 3',
+        tabText: 'Texto do terceiro tab'
+    },
+    {
+        tabName: 'tab 4',
+        tabText: 'Texto do quarto tab'
+    },
+]
+
+const tabNames = tabsJSON.map((tab) => tab.tabName)
+const tabTexts = tabsJSON.map((tab) => tab.tabText)
+
+export function Tab(){
+    const [currentTab, setCurrentTab] = useState(0)
+
+    function select(tabIndex: number) {
+        setCurrentTab(tabIndex)
+        console.log("Selecionou " + tabIndex)
+    }
+
+
+    const tabss = (
+        <div>
+            <TabItem
+                tabs ={ tabNames } 
+                text = { tabTexts }
+                onSelect = {select}
+                selection= {currentTab}
+            />
+        </div>
+    )
+
+    return(
+        tabss
+    )
+}
